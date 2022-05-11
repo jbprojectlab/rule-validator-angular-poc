d289e6c16fa91378534557ffca904d5c90ef56b0
jbprojectlab <james.byrd1265@gmail.com>
2022-05-11 00:00:01 -0400

    refactor: mock table data

--- src/app/certification-report/certification-report.component.ts
+++ src/app/certification-report/certification-report.component.ts
@@ -11,6 +11,16 @@ export class CertificationReportComponent implements OnInit {
   products: any = productData
   menuItems: any = []
   menuIsOpen: boolean = false
+  // expandedTableIndexes: any[] = productData.map(({data, data2, data3}, idx) => ({
+  //   idx,
+  //   data,
+  //   data2,
+  //   data3
+  // }))
+
+  expandedTableIndexes: any[] = []
+
+  // expandedTableIndexes: any[] = productData.map((product, idx) => product.tables.map((table, idx) => idx))
 
   openMenu() {
     if(!this.menuIsOpen) {
@@ -23,12 +33,43 @@ export class CertificationReportComponent implements OnInit {
       this.menuIsOpen = false
     }
   }
+
+  showMore(idx: number) {
+    this.expandedTableIndexes.push(idx)
+  }
+  
+  showLess(idx: number) {
+    this.expandedTableIndexes = this.expandedTableIndexes.filter(i => i !== idx)
+    console.log('showing less:  ', this.expandedTableIndexes)
+  }
+
+  logTableRows() {
+    for(let i = 0; i < this.products.length; i += 1) {
+      let product = this.products[i]
+      console.log('product title:  ', product.title)
+
+      for(let j = 0; j < product.tables.length; j += 1) {
+        let table = product.tables[j]
+        
+        for(let k = 0; k < table.length; k += 1) {
+
+          console.log('subtable:   ', table)
+          
+          for(let m = 0; m < table[0].length; m += 1) {
+            console.log('header:    ', table[0][m])
+          }
+
+          console.log('row:   ', table[k])
+        }
+      }
+    }
+  }
   
   getMenuItems() {
     // [{title:  'Product 1', items: ['foo', 'bar']}]
     const menuItems: any = []
-    for(let i = 0; i < this.products.length; i += 1) {
-      let product = this.products[i]
+    for(let i = 0; i < reportData.length; i += 1) {
+      let product = reportData[i]
       let section: any = {}
       section.title = product.name
       section.items = Object.keys(product.data)
@@ -41,5 +82,6 @@ export class CertificationReportComponent implements OnInit {
 
   ngOnInit(): void {
     this.menuItems = this.getMenuItems()
+    this.logTableRows()
   }
 }
