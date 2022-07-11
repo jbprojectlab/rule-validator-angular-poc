5067b2208fb85cb03e3d1e50c2fe79cbb337194d
jbprojectlab <james.byrd1265@gmail.com>
2022-07-11 10:34:24 -0400

    feat: add test coverage

--- src/app/pages/plan/components/plan-table/plan-table.component.spec.ts
+++ src/app/pages/plan/components/plan-table/plan-table.component.spec.ts
@@ -22,4 +22,49 @@ describe('PlanTableComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('format decimal number to percent string', () => {
+    it('should return "100%" when input is the whole number 1', () => {
+      expect(component.formatDecimalToPercent(1)).toBe('100.00%');
+    })
+
+    it('should return percent string greater than "100%" when input is greater than 1', () => {
+      expect(component.formatDecimalToPercent(1.27)).toBe('127.00%');
+    })
+
+    it('should return more than 200% when input is the whole number 2', () => {
+      expect(component.formatDecimalToPercent(2)).toBe('200.00%');
+    })
+
+    it('should return correct percent string when input is a decimal to the tens place', () => {
+      expect(component.formatDecimalToPercent(0.5)).toBe('50.00%');
+    })
+
+    it('should return correct percent string when input is a decimal to the hundreds place', () => {
+      expect(component.formatDecimalToPercent(0.75)).toBe('75.00%');
+    })
+
+    it('should return correct percent string when input is a repeating decimal', () => {
+      expect(component.formatDecimalToPercent(0.3333333333333333333333)).toBe('33.33%');
+    })
+
+    it('should return "0%" when input is a 0', () => {
+      expect(component.formatDecimalToPercent(0)).toBe('0.00%');
+    })
+  })
 });
+
+
+/*
+1
+
+2
+
+0.5
+
+0.75
+
+0.33
+
+0
+*/
